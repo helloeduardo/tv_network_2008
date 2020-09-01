977c0bdfc65884c624bce8a17699c536e5a7a115
Eduardo Parra <helloeduardo@icloud.com>
2020-09-01 09:17:03 -0600

    Fix initialize

--- lib/character.rb
+++ lib/character.rb
@@ -2,7 +2,6 @@ class Character
   attr_reader :name, :actor, :salary
 
   def initialize(character)
-    @character = character
     @name = character[:name]
     @actor = character[:actor]
     @salary = character[:salary]
