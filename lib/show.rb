304aa99fb9e3285074081089edb702bf8eb472e4
Eduardo Parra <helloeduardo@icloud.com>
2020-09-01 09:22:09 -0600

    Add total salary and test

--- lib/show.rb
+++ lib/show.rb
@@ -1,10 +1,16 @@
 class Show
   attr_reader :name, :creator, :characters
-  
+
   def initialize(name, creator, characters)
     @name = name
     @creator = creator
     @characters = characters
+  end
+
+  def total_salary
+    characters.sum do |character|
+      character.salary
+    end
   end
 
 end
