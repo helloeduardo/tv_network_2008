87f39467b2c42feea7e1321857c7e9e87565a138
Eduardo Parra <helloeduardo@icloud.com>
2020-09-01 09:09:12 -0600

    Add attributes and test

--- lib/character.rb
+++ lib/character.rb
@@ -1,8 +1,11 @@
-class CharacterTest
+class Character
   attr_reader :name, :actor, :salary
 
   def initialize(character)
     @character = character
+    @name = character[:name]
+    @actor = character[:actor]
+    @salary = character[:salary]
   end
 
 end
